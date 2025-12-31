074a6b82c3b0bc13a8745ff9cc9751baa79b2f2a
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2025-12-31 18:04:46 +0530

    Add files via upload

---
+++ Aggregation in sql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE marks(
+student_id INT,
+subject VARCHAR(20),
+score INT
+);
+
+SELECT * FROM marks;
+
+INSERT INTO marks VALUES
+(1,'Maths',85),
+(1,'Science',90),
+(2,'Maths',70),
+(2,'Science',75),
+(3,'Maths',95);
+
